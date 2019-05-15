040cee5b4ad460be850f29b69e9e1cbb4cb30e1f
Loyi <loyi.hsu@gmail.com>
2019-05-15 16:29:26 +0800

    Links to dedicated Loyiworks Project page and remove the files in preparation of the final LWKS site.

--- construction/loyiworks/javascript/navbar.js
+++
@@ -1,8 +0,0 @@
-function showMenu() {
-    var menu = document.getElementById("menu");
-    if (menu.style.display == "") {
-        menu.style.display = "block";
-    } else {
-        menu.style.display = "";
-    }
-}
--- construction/readme.md
+++
@@ -1,3 +0,0 @@
-# Construction Folder
-
-In this folder is the current snapshot of the website for cross device testing.
